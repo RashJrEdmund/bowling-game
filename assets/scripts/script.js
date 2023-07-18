fef2ae24be26072d62a305a4597b911125c06137
rash <orashusedmund@gmail.com>
2023-07-18 06:36:10 +0100

    done with updates

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,9 @@
 const score = document.querySelectorAll('.score')
 const totalScore = document.querySelectorAll('.total-score')
+const sum = document.querySelectorAll('.sum')
 const rollBtn = document.querySelector('.btn')
 const resetBtn = document.querySelector('.btn1')
 const loading = document.querySelector('.loading')
-const totalDisplay = document.querySelector('.total')
 
 let count = 0
 let totalScores = 0
@@ -22,35 +22,44 @@ const rollBall = () => {
 
   if (count === 10) {
     score[0].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
+    sum[0].innerHTML = `${pinsHitFirst + pinsHitSecond} `
     totalScore[0].innerHTML = totalHits
   } else if (count === 20) {
     score[1].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
+    sum[1].innerHTML = `${pinsHitFirst + pinsHitSecond} `
     totalScore[1].innerHTML = totalScores
   } else if (count === 30) {
     score[2].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
+    sum[2].innerHTML = `${pinsHitFirst + pinsHitSecond} `
     totalScore[2].innerHTML = totalScores
   } else if (count === 40) {
     score[3].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
+    sum[3].innerHTML = `${pinsHitFirst + pinsHitSecond} `
     totalScore[3].innerHTML = totalScores
   } else if (count === 50) {
     score[4].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
+    sum[4].innerHTML = `${pinsHitFirst + pinsHitSecond} `
     totalScore[4].innerHTML = totalScores
   } else if (count === 60) {
     score[5].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
+    sum[5].innerHTML = `${pinsHitFirst + pinsHitSecond} `
     totalScore[5].innerHTML = totalScores
   } else if (count === 70) {
     score[6].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
+    sum[6].innerHTML = `${pinsHitFirst + pinsHitSecond} `
     totalScore[6].innerHTML = totalScores
   } else if (count === 80) {
     score[7].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
+    sum[7].innerHTML = `${pinsHitFirst + pinsHitSecond} `
     totalScore[7].innerHTML = totalScores
   } else if (count === 90) {
     score[8].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
+    sum[8].innerHTML = `${pinsHitFirst + pinsHitSecond} `
     totalScore[8].innerHTML = totalScores
   } else if (count === 100) {
     score[9].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
+    sum[9].innerHTML = `${pinsHitFirst + pinsHitSecond} `
     totalScore[9].innerHTML = totalScores
-    totalDisplay.innerHTML = `Total Pins: ${totalScores}`
   } else {
     loading.innerHTML = 'You have exceeded the number of frames'
     loading.classList.add('active')
